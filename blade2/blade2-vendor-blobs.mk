4d7066857959169fd216f258a993fd12465ff442
Daz J <yuki@thebronasium.com>
2012-04-29 15:56:34 +0100

    Remove unnecessary file

--- blade2/blade2-vendor-blobs.mk
+++ blade2/blade2-vendor-blobs.mk
@@ -39,8 +39,6 @@ PRODUCT_COPY_FILES := \
     vendor/zte/blade2/proprietary/lib/egl/eglsubAndroid.so:obj/lib/egl/eglsubAndroid.so \
     vendor/zte/blade2/proprietary/lib/egl/libEGL_adreno200.so:system/lib/egl/libEGL_adreno200.so \
     vendor/zte/blade2/proprietary/lib/egl/libEGL_adreno200.so:obj/lib/egl/libEGL_adreno200.so \
-    vendor/zte/blade2/proprietary/lib/egl/libGLES_android.so:system/lib/egl/libGLES_android.so \
-    vendor/zte/blade2/proprietary/lib/egl/libGLES_android.so:obj/lib/egl/libGLES_android.so \
     vendor/zte/blade2/proprietary/lib/egl/libGLESv1_CM_adreno200.so:system/lib/egl/libGLESv1_CM_adreno200.so \
     vendor/zte/blade2/proprietary/lib/egl/libGLESv1_CM_adreno200.so:obj/lib/egl/libGLESv1_CM_adreno200.so \
     vendor/zte/blade2/proprietary/lib/egl/libGLESv2_adreno200.so:system/lib/egl/libGLESv2_adreno200.so \
